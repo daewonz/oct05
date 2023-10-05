518940ebd1de152ee31fcda6257062d3712006f6
daewonz <korea96440@naver.com>
2023-10-05 20:47:41 +0900

    2023-10-05

---
+++ sep19/src/main/java/com/ddww/Sep19Application.java
@@ -0,0 +1,13 @@
+package com.ddww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep19Application.class, args);
+	}
+
+}
